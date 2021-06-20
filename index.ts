1330aab6c38491d102b49604a008fa161407d09a
Ivan <miltonivan117@gmail.com>
2021-06-20 09:06:11 -0600

    APIV1.0

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { data } from "./src/assets/data";
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.static("public"));
+
+app.use("/images", express.static("images"));
+
+app.get("/", (req, res) => res.send(data));
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
---
+++ src/assets/data.ts
@@ -0,0 +1,132 @@
+export const data = [
+  {
+    id: 1,
+    title: "Senior Frontend Developer",
+    company: {
+      name: "Photosnap",
+      logo: `http://localhost:3000/images/photosnap.svg`,
+    },
+    descript: {
+      ago: "1d ago",
+      time: "Full Time",
+      only: "USA Only",
+    },
+  },
+  {
+    id: 2,
+    title: "Fullstack Developer",
+    company: {
+      name: "Manage",
+      logo: `http://localhost:3000/images/manage.svg`,
+    },
+    descript: {
+      ago: "1d ago",
+      time: "Part Time",
+      only: "Remote",
+    },
+  },
+  {
+    id: 3,
+    title: "Junior Frontend Developer",
+    company: {
+      name: "Account",
+      logo: `http://localhost:3000/images/account.svg`,
+    },
+    descript: {
+      ago: "2d ago",
+      time: "Part Time",
+      only: "USA Only",
+    },
+  },
+  {
+    id: 4,
+    title: "Junior Frontend Developer",
+    company: {
+      name: "MyHome",
+      logo: `http://localhost:3000/images/myhome.svg`,
+    },
+    descript: {
+      ago: "5d ago",
+      time: "Contract",
+      only: "USA Only",
+    },
+  },
+  {
+    id: 5,
+    title: "Software Engineer",
+    company: {
+      name: "Loop Studios",
+      logo: `http://localhost:3000/images/loop-studios.svg`,
+    },
+    descript: {
+      ago: "1w ago",
+      time: "Full Time",
+      only: "WorldWide",
+    },
+  },
+  {
+    id: 6,
+    title: "Junior Backend Developer",
+    company: {
+      name: "FaceIt",
+      logo: `http://localhost:3000/images/faceit.svg`,
+    },
+    descript: {
+      ago: "2w ago",
+      time: "Full Time",
+      only: "UK Only",
+    },
+  },
+  {
+    id: 7,
+    title: "Junior Developer",
+    company: {
+      name: "Shortly",
+      logo: `http://localhost:3000/images/shortly.svg`,
+    },
+    descript: {
+      ago: "2w ago",
+      time: "Full Time",
+      only: "WorldWide",
+    },
+  },
+  {
+    id: 8,
+    title: "Junior Frontend Developer",
+    company: {
+      name: "Insure",
+      logo: `http://localhost:3000/images/insure.svg`,
+    },
+    descript: {
+      ago: "2w ago",
+      time: "Full Time",
+      only: "Usa Only",
+    },
+  },
+  {
+    id: 9,
+    title: "Full Stack Engineer",
+    company: {
+      name: "Eyecam Co.",
+      logo: `http://localhost:3000/images/eyecam-co.svg`,
+    },
+    descript: {
+      ago: "3w ago",
+      time: "Full Time",
+      only: "WorldWide",
+    },
+  },
+  {
+    id: 10,
+    title: "Front-end Dev",
+    company: {
+      name: "The Air Filter Company",
+      logo: `http://localhost:3000/images/the-air-filter-company.svg`,
+    },
+    descript: {
+      ago: "1mo ago",
+      time: "Part Time",
+      only: "WorldWide",
+    },
+  },
+];
