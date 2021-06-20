0cf2ea6b30e502ea7c12cff122c218dd2a8841dc
Ivan <miltonivan117@gmail.com>
2021-06-20 11:01:17 -0600

    app v2.1

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
-import express from "express";
+const express = require('express')
 import { data } from "./assets/data";
 
 const app = express();
 const PORT = 3000;
 
-
 app.use(express.static("public"));
 
 app.use("/images", express.static("images"));
 
-app.get("/", (req, res) => res.send(data));
+app.get(
+  "/",
+  (
+    req: any,
+    res: {
+      send: (
+        arg0: {
+          id: number;
+          title: string;
+          company: { name: string; logo: string };
+          descript: { ago: string; time: string; only: string };
+        }[]
+      ) => any;
+    }
+  ) => res.send(data)
+);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
