03e15269d0fef85745e3e72faf836c6ad2561734
Ivan <miltonivan117@gmail.com>
2021-06-21 10:21:00 -0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const data = require("../data.json");
+
 const PORT = 8000;
 
-const data = require("../data.json");
-console.log(data);
-
+app.use(cors());
 app.use(express.static("public"));
-
 app.use("/images", express.static("images"));
 
-app.get("/", (req, res) => {
-  res.json(data)
-  res.sendStatus(200).send('OK')
+app.get("/", (req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: any): void; new(): any; }; }; } ) => {
+  res.status(200).json(data)
 });
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
+
+module.exports = app;
