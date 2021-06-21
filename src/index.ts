c619d9896688d95e40857eaafc448820312b72f7
Ivan <miltonivan117@gmail.com>
2021-06-21 10:49:53 -0600

    Appv2.5

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const data = require("../data.json");
+const data = require("./data.json");
 
 const PORT = 8000;
 
